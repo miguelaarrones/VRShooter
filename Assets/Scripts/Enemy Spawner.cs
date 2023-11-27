aa940c6d67c76203d54051136b820975d6ac42a0
Miguel A. Arrones <miguelaarrones@icloud.com>
2023-11-27 18:46:37 +0100

    Added Enemy Spawner and basic Enemy AI

---
+++ Assets/Scripts/Enemy Spawner.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private EnemyAI enemyPrefab;
+    [SerializeField] private float spawnInterval;
+    [SerializeField] private int maxEnemiesNumber;
+    [SerializeField] private Player player;
+
+    private List<EnemyAI> spawnedEnemies = new List<EnemyAI>();
+    private float timeSinceLastSpawn;
+
+    private void Start()
+    {
+        timeSinceLastSpawn = spawnInterval;
+    }
+
+    private void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn > spawnInterval)
+        {
+            timeSinceLastSpawn = 0;
+            if (spawnedEnemies.Count < maxEnemiesNumber)
+            {
+                SpawnEnemy();
+            }
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        EnemyAI enemy = Instantiate(enemyPrefab, transform.position, transform.rotation);
+        int spawnPointIndex = spawnedEnemies.Count % spawnPoints.Length;
+        enemy.Init(player, spawnPoints[spawnPointIndex]);
+
+        spawnedEnemies.Add(enemy);
+    }
+}
